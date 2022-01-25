40ccbb474e2a9c9491411f4cd8cbefe920e5d4fa
maggiefitzz <mfitzgerald@eastsideprep.org>
2022-01-25 14:56:05 -0800

    petv1

---
+++ virtualpet/js/pet.js
@@ -0,0 +1,11 @@
+function feedPet() {
+    console.log("feeding doggie...")
+}
+
+function letOut() {
+    console.log("letting out doggie...")
+}
+
+function tireDog() {
+    console.log("tiring out doggie...")
+}
